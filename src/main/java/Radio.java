f7f0bec038fddea72233abf87516bfafad77a5e2
Topce <425363@mail.ru>
2023-03-01 00:19:54 +0700

    Add new parameters

---
+++ src/main/java/Radio.java
@@ -0,0 +1,54 @@
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int maxStation;
+    public Radio () {
+        maxStation = 9;
+    }
+    public Radio(int stationsCount) {
+        maxStation = stationsCount - 1;
+    }
+
+    public void nextStation() {
+        if (currentStation != maxStation) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+    public void prevStation() {
+        if (currentStation != 0) {
+            currentStation--;
+        } else {
+            currentStation = maxStation;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > maxStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 100) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+}
